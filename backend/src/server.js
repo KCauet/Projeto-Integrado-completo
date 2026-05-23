ea298bfc597285cc7d57fc5e5dee58899cac5cac
Keirrison cauet <keirrisoncauet31@gmail.com>
2026-05-23 14:00:19 -0300

    Conexão com o banco de dados MongoDB além de implementações no server.js

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import app from "./app.js"
+import dotenv from "dotenv"
+import connectDB from "./database/connect.js"
+
+dotenv.config()
+
+connectDB()
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
